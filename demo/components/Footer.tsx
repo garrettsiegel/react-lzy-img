774993d781864e1b6b63f6ef991012c77fe94a38
garrettsiegel <garrett@garrettsiegel.com>
2026-01-10 14:13:04 -0500

    name in footer

--- demo/components/Footer.tsx
+++ demo/components/Footer.tsx
@@ -113,7 +113,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-700/50 mt-12 pt-8 text-center">
-          <p className="text-gray-500">© 2026 react-lzy-img. All rights reserved.</p>
+          <p className="text-gray-500">© 2026 Garrett Siegel. All rights reserved.</p>
         </div>
       </div>
     </footer>
